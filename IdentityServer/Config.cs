d3879e925ee355f6502ab38c6bc42ee37f48e8c5
Chandima Ranaweera <chan4lk@gmail.com>
2021-01-02 00:37:28 +0530

    Identity server added

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("apiscope", "Read Access to API #1")
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myapiresource", "My Api Resource")
+                {
+                    Scopes = { "apiscope"  }
+                }
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "secret_client_id",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "apiscope"}
+                }
+            };
+        }
+    }
+}
